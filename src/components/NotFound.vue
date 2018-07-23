3009a9b044fe341763d4d8c8e92cdd4e242630b3
ian morton <ian.happytuesdays@gmail.com>
2018-07-23 20:44:09 +0100

    Different threejs init not working

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -10,31 +10,47 @@
 </template>
 
 <script>
-const THREE = require('three')
+import * as Three from 'three'
 
 export default {
-
+  name: 'ThreeTest',
+  data () {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null
+    }
+  },
   methods: {
     init: function () {
-      var scene = new THREE.Scene()
-      var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+      var container = document.getElementById('three')
 
-      var renderer = new THREE.WebGLRenderer()
-      renderer.setSize(window.innerWidth, window.innerHeight)
-      var threeWin = document.getElementById('three')
-      threeWin.appendChild(this.renderer.domElement)
+      this.camera = new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10)
+      this.camera.position.z = 1
 
-      var geometry = new THREE.BoxGeometry(1, 1, 1)
-      var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-      var cube = new THREE.Mesh(geometry, material)
-      scene.add(cube)
+      this.scene = new Three.Scene()
 
-      camera.position.z = 5
+      var geometry = new Three.BoxGeometry(0.2, 0.2, 0.2)
+      var material = new Three.MeshNormalMaterial()
+
+      this.mesh = new Three.Mesh(geometry, material)
+      this.scene.add(this.mesh)
+
+      this.renderer = new Three.WebGLRenderer({antialias: true})
+      this.renderer.setSize(container.clientWidth, container.clientHeight)
+      container.appendChild(this.renderer.domElement)
+    },
+    animate: function () {
+      requestAnimationFrame(this.animate)
+      this.mesh.rotation.x += 0.01
+      this.mesh.rotation.y += 0.02
+      this.renderer.render(this.scene, this.camera)
     }
   },
   mounted () {
     this.init()
-    // this.animate()
+    this.animate()
   }
 }
 </script>
